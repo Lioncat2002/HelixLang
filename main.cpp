15d796fbb8bb6b76648a3fda822a65463d59d79c
Lioncat2002 <sankhayan2002@gmail.com>
2024-08-16 23:57:15 +0530

    feat: added a bit more polished cli

--- main.cpp
+++ main.cpp
@@ -1,14 +1,95 @@
+#include <cstdlib>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <llvm-14/llvm/IR/Module.h>
 #include <llvm/Support/raw_ostream.h>
 #include <sstream>
+#include <system_error>
+#include <utility>
 #include "src/core/lexer/Lexer.h"
 #include "src/core/parser/Parser.h"
 #include "src/core/sema/Sema.h"
 #include "src/core/codegen/Codegen.h"
-
+#include "src/utils/Driver.h"
+#include "src/utils/Utils.h"
 
 int main(int argc, const char **argv) {
+    hlx::CompilerOptions options=hlx::parseArguments(argc, argv);
+
+    if(options.displayHelp){
+        hlx::displayHelp();
+        return 0;
+    }
+    if(options.source.empty())
+        hlx::error("no source file empty");
+
+    if(options.source.extension()!=".hlx")
+        hlx::error("unexpected source file extension");
+    
+    std::ifstream file(options.source);
+    if(!file)
+        hlx::error("failed to open '"+options.source.string()+'\'');
+
+    std::stringstream buffer;
+    buffer<<file.rdbuf();
+    hlx::SourceFile sourceFile={options.source.c_str(),buffer.str()};
+
+    hlx::Lexer lexer(sourceFile);
+    hlx::Parser parser(lexer);
+
+    auto [ast,success]=parser.parseSourceFile();
+
+    if(options.astDump){
+        for(auto &&fn:ast){
+            fn->dump();
+        }
+        return 0;
+    }
+
+    if(!success)
+        return 1;
+
+    hlx::Sema sema(std::move(ast));
+    auto resolvedTree=sema.resolveAST();
+
+    if(options.resDump){
+        for(auto &&fn:resolvedTree)
+            fn->dump();
+        return 0;
+    }
+
+    if (resolvedTree.empty()) {
+        return 1;
+    }
+
+    hlx::Codegen codegen(std::move(resolvedTree),options.source.c_str());
+
+    llvm::Module *llvmIR=codegen.generateIR();
+    if(options.llvmDump){
+        llvmIR->dump();
+        return 0;
+    }
+
+    std::stringstream path;
+    path<<"tmp-"<<std::filesystem::hash_value(options.source)<<".ll";
+    const std::string &&llvmIRPath=path.str();
+
+    std::error_code errorCode;
+    llvm::raw_fd_ostream f(llvmIRPath,errorCode);
+    llvmIR->print(f, nullptr);
+
+    std::stringstream command;
+    command<<"clang "<<llvmIRPath;
+    if(!options.output.empty())
+        command<<"-o"<<options.output;
+
+    int ret=std::system(command.str().c_str());
+
+    std::filesystem::remove(llvmIRPath);
+
+    return ret;
+    /*
     std::ifstream file(argv[1]);
     if(!file){
         std::cerr<<"Couldn't find file: "<<argv[1]<<'\n';
@@ -58,4 +139,5 @@ int main(int argc, const char **argv) {
     //codegen.generateIR()->print(llvm::errs(), nullptr);
 
     return !success;
+    */
 }
---
+++ src/utils/Driver.cpp
@@ -0,0 +1,54 @@
+#include "Driver.h"
+#include <cstdlib>
+#include <iostream>
+#include <string_view>
+
+namespace hlx {
+CompilerOptions parseArguments(int argc, const char **argv) {
+  CompilerOptions options;
+
+  int idx = 1;
+  while (idx < argc) {
+    std::string_view arg = argv[idx];
+
+    if (arg[0] != '-') {
+      if (!options.source.empty()) {
+        error("unexpected argument '" + std::string(arg) + '\'');
+      }
+      options.source = arg;
+    } else {
+      if (arg == "-h")
+        options.displayHelp = true;
+      else if (arg == "-o")
+        options.output = ++idx >= argc ? "" : argv[idx];
+      else if (arg == "-ast-dump")
+        options.astDump = true;
+      else if (arg == "-llvm-dump")
+        options.llvmDump = true;
+      else if (arg == "-cfg-dump")
+        options.cfgDump = true;
+      else
+        error("unexpected option '" + std::string(arg) + '\'');
+    }
+    ++idx;
+  }
+
+  return options;
+}
+
+[[noreturn]] void error(std::string_view msg) {
+  std::cerr << "error: " << msg << '\n';
+  std::exit(1);
+}
+
+void displayHelp() {
+  std::cout << "Usage:\n"
+            << "  compiler [options] <source_file>\n\n"
+            << "Options:\n"
+            << "  -h           display this message\n"
+            << "  -o <file>    write executable to <file>\n"
+            << "  -ast-dump    print the abstract syntax tree\n"
+            << "  -res-dump    print the resolved syntax tree\n"
+            << "  -llvm-dump   print the llvm module\n";
+}
+} // namespace hlx
